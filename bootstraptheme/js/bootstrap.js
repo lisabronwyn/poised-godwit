fb4de875b18269c9ef397d64741c7c2a3acc2f2e
Lisa Bronwyn <e.bronwyn@gmail.com>
2016-11-04 11:30:49 -0700

    design tweaks

--- bootstraptheme/js/bootstrap.js
+++ bootstraptheme/js/bootstrap.js
@@ -80,9 +80,6 @@ if (typeof jQuery === 'undefined') {
  * Bootstrap: alert.js v3.3.7
  * http://getbootstrap.com/javascript/#alerts
  * ========================================================================
- * Copyright 2011-2016 Twitter, Inc.
- * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
- * ======================================================================== */
 
 
 +function ($) {
